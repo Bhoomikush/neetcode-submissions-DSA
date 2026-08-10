b6b25445fdc9710ca4bf077d11496064409e098d
Bhoomi Kushwah <144367605+Bhoomikush@users.noreply.github.com>
2026-08-10 08:06:40 +0530

    Update: top-k-elements-in-list - submission-0

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
@@ -1,36 +1,31 @@
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+
+        // 1. Count frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue <Integer> pq = new PriorityQueue<>(
-            (a,b)->map.get(a)-map.get(b)
+        // 2. Max heap based on frequency
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> map.get(b) - map.get(a)
         );
 
-        for(int num : map.keySet()){
-            pq.offer(num);
-
-            
-            if(pq.size()>k){
-            pq.poll();
-            }
+        // Add all unique numbers
+        for (int num : map.keySet()) {
+            pq.add(num);
         }
 
-        int [] ans = new int[k];
-        for(int i=0;i<k;i++){
-            ans[i]=pq.poll();
+        // 3. Get top k frequent elements
+        int[] result = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll();
         }
-        return ans;
 
-
-
-
-        
-
-       
-
+        return result;
     }
 }
